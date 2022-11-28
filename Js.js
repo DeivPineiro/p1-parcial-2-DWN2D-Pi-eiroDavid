ad5ef99c13e9cb50373c91d39806941cedf245a0
DeivPineiro <david.pineiro89@gmail.com>
2022-11-28 09:42:26 -0300

    Se agregar mostrar productos por DOM

--- Js.js
+++ Js.js
@@ -9,7 +9,7 @@ let productos = [
         id: 1,
         nombre: 'Producto 1',
         descripcion: 'Descripción del producto 1',
-        precio: 0,
+        precio: 10,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'A',
     },
@@ -17,7 +17,7 @@ let productos = [
         id: 2,
         nombre: 'Producto 2',
         descripcion: 'Descripción del producto 2',
-        precio: 0,
+        precio: 20,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'A',
     },
@@ -25,7 +25,7 @@ let productos = [
         id: 3,
         nombre: 'Producto 3',
         descripcion: 'Descripción del producto 3',
-        precio: 0,
+        precio: 30,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'B',
     },
@@ -33,7 +33,7 @@ let productos = [
         id: 4,
         nombre: 'Producto 4',
         descripcion: 'Descripción del producto 4',
-        precio: 0,
+        precio: 40,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'B',
     },
@@ -41,7 +41,7 @@ let productos = [
         id: 5,
         nombre: 'Producto 5',
         descripcion: 'Descripción del producto 5',
-        precio: 0,
+        precio: 50,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'C',
     },
@@ -49,13 +49,52 @@ let productos = [
         id: 6,
         nombre: 'Producto 6',
         descripcion: 'Descripción del producto 6',
-        precio: 0,
+        precio: 60,
         imagen: 'producto-de-ejemplo.jpg',
         categoría: 'C',
     },
 
 ];
 
+const unProducto = productos[1];
 
+console.log(unProducto);
+
+const listaProductos = document.getElementById("listaDeProductos");
+
+
+//tarjeta
 const tarjeta = document.createElement('div');
+tarjeta.setAttribute ("class", "tarjeta");
+tarjeta.setAttribute ("id", unProducto.id)
+listaProductos.appendChild(tarjeta);
+
+//imagen
+const imagenProducto = document.createElement('img');
+imagenProducto.setAttribute ("src", "producto-de-ejemplo.jpg");
+imagenProducto.setAttribute ("alt", "producto-de-ejemplo");
+tarjeta.appendChild(imagenProducto);
+
+//titulo
+const tituloTarjeta = document.createElement('h3');
+tituloTarjeta.innerText = unProducto.nombre;
+tarjeta.appendChild(tituloTarjeta);
+
+//descripcion
+const descripcionProducto = document.createElement('p');
+descripcionProducto.innerText = unProducto.descripcion;
+tarjeta.appendChild(descripcionProducto);
+
+//precio
+const precioProducto = document.createElement('p')
+precioProducto.innerText = "Precio: " + unProducto.precio;
+tarjeta.appendChild(precioProducto);
+
+//Categoria
+const categoriaProducto = document.createElement('p')
+categoriaProducto.innerText = "Categoria: " + unProducto.categoría;
+tarjeta.appendChild(categoriaProducto);
+
+
+
 
